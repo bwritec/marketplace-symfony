ab60cad841264b498b59bd42d3d9093761ec4ff8
Jorge Luiz <bjxjorge@gmail.com>
2026-05-24 22:17:35 -0300

    .

---
+++ migrations/Version20260525011530.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260525011530 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Cria tabela product_characteristics';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('
+            CREATE TABLE product_characteristics (
+                id INT AUTO_INCREMENT NOT NULL,
+                user_id INT NOT NULL,
+                product_id INT NOT NULL,
+                characteristic VARCHAR(255) DEFAULT NULL,
+                PRIMARY KEY(id)
+            ) DEFAULT CHARACTER SET utf8mb4
+            COLLATE `utf8mb4_unicode_ci`
+            ENGINE = InnoDB
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE product_characteristics');
+    }
+}
